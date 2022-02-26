70a291bb7c1b93fa68b40fbd73503ac70195c703
nd <andy.spizhavka@belkins.io>
2022-02-27 01:51:56 +0200

    add some styles

--- app.js
+++ app.js
@@ -2,17 +2,20 @@ import { Telegraf, Markup } from "telegraf";
 import axios from "axios";
 
 const token = "5227368679:AAG6NMaoWxG7AOYcnpK0eMigxOYa6vwYpsU";
+
+const bot = new Telegraf(token);
+
 const domain = "";
 const ORG_ID = "organisationId__";
 const CITY_ID = "cityId__";
 
-let organisations = [
+const organisations = [
   {
     name: "Test1",
     tableId: 1,
     city: "Kyiv",
     contacts: {
-      tel: "+380994584994",
+      tel: "+3123123",
       address: "бла бла бла",
       tg: "",
       facebook: "",
@@ -42,12 +45,9 @@ let organisations = [
   },
 ];
 
-const bot = new Telegraf(token);
-
 function getCities(cities) {
   organisations.forEach((org) => {
-    if (!cities.includes(org.city))
-      cities.push({ city: org.city, id: CITY_ID + org.city });
+    if (!cities.includes(org.city)) cities.push(org.city);
   });
 }
 
@@ -55,20 +55,54 @@ bot.start((ctx) => {
   let cities = [];
   getCities(cities);
   ctx.reply(
-    "Привіт, цей бот створено для того, щоб ти зміг легко дізнатися, кому і яка потрібна допомога.\n Для початку обери місто:",
+    "Привіт, цей бот створено для того, щоб ти зміг легко дізнатися, кому і яка потрібна допомога.\n" +
+      "Для початку обери місто:",
     Markup.inlineKeyboard(
-      cities.map((o) => [Markup.button.callback(o.city + " 🇺🇦", o.id)])
+      cities.map((city) => [
+        Markup.button.callback(city + " 🇺🇦", CITY_ID + city),
+      ])
     )
   );
 });
 
 bot.action(new RegExp(ORG_ID + "*"), (ctx) => {
-  //   axios.get(`${domain}/`).then((response) => {
-  console.log(ctx);
-  ctx.reply(
-    `Назва організації:  \nКонтакти: \nТелефон: \nАдреса: \nТелеграм: \nФейсбук: \nОсь що нам потрібно:`
+  const organisationInfo = organisations.find(
+    (org) => org.tableId == ctx.match.input.replace(ORG_ID, "")
   );
-  //   });
+  //   axios.get(`${domain}/`).then((response) => {
+  const test = [
+    {
+      value: "25",
+      name: "Бронежилет",
+    },
+    {
+      value: "12",
+      name: "Что-то",
+      description: "lorem ipsum",
+    },
+  ];
+
+  const reformatData = test.map(
+    (item) =>
+      "❤️" +
+      item.name +
+      " " +
+      item.value +
+      " " +
+      (item.description ? item.description : " ") +
+      "\n"
+  );
+
+  ctx.reply(
+    `Назва організації: ${organisationInfo.name} \n` +
+      `Контакти: \n` +
+      `📱Телефон: ${organisationInfo.contacts.tel}\n` +
+      `📍Адреса: ${organisationInfo.contacts.address}\n` +
+      `🔵Телеграм: ${organisationInfo.contacts.tg} \n` +
+      `🔵Фейсбук: ${organisationInfo.contacts.facebook}\n\n` +
+      `Ось що нам потрібно: \n\n` +
+      `${reformatData}`
+  );
 });
 
 bot.action(new RegExp(CITY_ID + "*"), (ctx) => {
@@ -76,8 +110,8 @@ bot.action(new RegExp(CITY_ID + "*"), (ctx) => {
     "Обери організацію",
     Markup.inlineKeyboard(
       organisations
-        .filter((o) => ctx.match.input.replace(CITY_ID, "") === o.city)
-        .map((o) => [Markup.button.callback(o.name, ORG_ID + o.id)])
+        .filter((org) => ctx.match.input.replace(CITY_ID, "") === org.city)
+        .map((org) => [Markup.button.callback(org.name, ORG_ID + org.tableId)])
     )
   );
 });
